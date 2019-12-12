860edbf4e749403f3bc008b2e5addd4c784aa82a
totung <v1forever.alone11@gmail.com>
2019-12-12 19:38:59 +0700

    con check email nua la xong

--- js/js.js
+++ js/js.js
@@ -24,4 +24,30 @@ function kiemtra(){
 		document.getElementById('22').innerText='Hợp lệ';
 	}
 	//check so dien thoai
+	var phone1= /^(09(6|7|8))|(03[2-9])|(086)\d{7}$/;
+	var phone = document.getElementById('phone').value;
+	var checkphone= phone.search(phone1);
+	if (phone==""){
+		document.getElementById('3').innerText='Không hợp lệ : số điện thoại không được để trống';		
+	}else if (checkphone==-1) {
+		document.getElementById('3').innerText='Không hợp lệ : số điện thoại không hợp lệ';	
+	}else{
+		document.getElementById('33').innerText='Hợp lệ';
+	}
+	// check email
+	var email1= /^$/;
+	var email = document.getElementById('email').value;
+	var checkemail= email.search(email1);
+	if (email==""){
+		document.getElementById('4').innerText='Không hợp lệ : email không được để trống';		
+	}else if (checkemail==-1) {
+		document.getElementById('4').innerText='Không hợp lệ : email không hợp cú pháp';	
+	}else{
+		document.getElementById('44').innerText='Hợp lệ';
+	}
+	//check gioi tính
+	//check khoa hoc dang ki
+	//check khoa hoc da tham gia
+	//check ghi chu
+	//check ma xac nhan
 }
